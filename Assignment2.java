b1e54bc52c4f6b50cdd7049a93fc3b048cb153ea
Matthew Montoya <msmontoya2@miners.utep.edu>
2019-02-26 22:19:25 -0700

    Inital Commit to Master Branch

---
+++ Assignment2.java
@@ -0,0 +1,33 @@
+/*
+ * CS 165a
+ * Lab 1: Tree Searches
+ * By: Matthew S Montoya && Hiram Rios
+ * Last Modified: 26 February 2019
+ * Purpose: To practicing implementing BFS, IDS, & A* Search
+ */
+
+ import java.io.*;          //For fileReader() & bufferedReader()
+ import java.util.*;        //For scanner
+
+public class Assignment2 {
+    public static void main(String[] args){
+        breadth_first_search();
+        iterative_deepening_search();
+        a_star_search();
+    }
+
+
+    public static void breadth_first_search(){
+        System.out.println("Hello");
+    }
+
+
+    public static void iterative_deepening_search(){
+        System.out.println("Hello");
+    }
+
+
+    public static void a_star_search(){
+        System.out.println("Hello");
+    }
+}
