f4bc0d20039084da6b74cff725849c90376eae38
Matthew Montoya <msmontoya2@miners.utep.edu>
2019-03-01 12:32:15 -0700

    Friday morning BFS improvements + 1st implementation of IDS

--- Assignment2.java
+++ Assignment2.java
@@ -2,15 +2,18 @@
  * CS 165a
  * Lab 1: Tree Searches
  * By: Matthew S Montoya && Hiram Rios
- * Last Modified: 26 February 2019
- * Purpose: To practicing implementing BFS, IDS, & A* Search
+ * Last Modified: 1 March 2019
+ * Purpose: To practicing implementing BFS, IDS, & A* Search Algorithms
  * NOTE: THIS IS THE RUNNER FILE
  */
 
  import java.io.*;          //For fileReader() & bufferedReader()
- import java.util.*;        //For scanner
+ import java.util.*;        //For scanner when TA executes .jar file
 
 public class Assignment2 {
+    //I fucked up. We'll need variable decalations before we initialize the methods
+    public static int children_expanded = 0;        //Counter for the number of expanded sucessor nodes
+    public static int memory_nodes = 0;             //Counter for our nodes in memory
 
     /**
 	 * GENERATE MAP SPACE
@@ -78,40 +81,49 @@ public class Assignment2 {
 	 * This method implements BFS on our tree from our text file.
 	 */
     public static void breadth_first_search(int[][] map_space){
+        //Dr. K --> USE A 3-MINUTE TIMER
+		long begin_timer = System.currentTimeMillis();
+        long end_timer = 180000;
+        boolean times_up = false;
+
         //Generate Goal Node
         Node goal_node = new Node(goal_position[0]-1, goal_position[1]-1);
         //Generate Starting Node
 		Node start_node = new Node(starting_position[0]-1, starting_position[1]-1, manhattan_distance(starting_position[0]-1, starting_position[1]-1, goal_node), map_space[starting_position[0]-1][starting_position[1]-1], null);
-        
-        int expanded_nodes = 0;
         boolean[][] nodes_visited = new boolean[map_space.length][map_space[0].length];
         Queue<Node> queue_nodes = new LinkedList<>();
 		queue_nodes.add(start_node);
-        Node[] child_node_array;
 
         while(!(queue_nodes.isEmpty())){
-
-            //Search && Expand the current node
-            Node current_node = queue_nodes.poll();
-			nodes_visited[current_node.x][current_node.y] = true;            //Call .x & .y position in Node class
-			expanded_nodes++;
-            
-            //If a goal exists, display path information
-			if(goalTest(current_node)){
-                int path_cost = current_node.accumulated_path_cost;
-				display_path_information(current_node, path_cost);
+            //Start the 3-Minute timer
+            if((System.currentTimeMillis()-begin_timer) > end_timer){
+				System.out.println("3 minutes have passed. Your search time has expired!");
+				times_up = true;
 				break;
 			}
-            
-            //If no goal exists, generate the children nodes & add them to the queue
-			child_node_array = generate_successor_nodes(current_node, goal_node, nodes_visited, map_space);//get children and add the to queue
-			for(int i=0; i<child_nodes.length; i++){
-                queue_nodes.add(child_nodes[i]);
+
+            //Search && Expand the current node
+            Node current_node = queue_nodes.poll();                             //Removes + Returns head of queue 
+			nodes_visited[current_node.x][current_node.y] = true;               //Call .x & .y position in Node class
+			memory_nodes++;
+
+            //Check if this is a goal node
+			if(is_goal_node(current_node)){
+                int path_cost = current_node.accumulated_path_cost;
+                display_path_information(current_node, path_cost);              //Display goal path information 
+                System.out.println("Our path cost: "+path_cost);
+				break;
+			}
+
+            //Generate Successor Nodes && add to the queue
+			Node[] children_nodes_array = generate_successor_nodes(current_node, goal_node, nodes_visited, map_space);
+			for(int i=0; i<children_nodes_array.length; i++){
+                queue_nodes.add(children_nodes_array[i]);
             }
-            
-            //FIXME: IMPLEMENT THE MAX NODES IN THE MEMORY
-			//if(maxNodesInMemory < queue_nodes.size())//add the children to count for nodes in memory
-			//	maxNodesInMemory = queue_nodes.size();
+
+            //Add sucessor nodes to memory
+            if(children_expanded<queue_nodes.size())           //Fuck it; what if we just make all the variables statically available to the entire program?
+                children_expanded = queue_nodes.size();
         }
     }
 
@@ -122,10 +134,100 @@ public class Assignment2 {
 	 * This method implements IDS on our tree from our text file.
 	 */
     public static void iterative_deepening_search(int[][] map_space){
-        //FIXME: The following call should take arguments similar to: generate_successor_nodes(Node current, Node g, boolean[][] visited, int[][] map)
-        //generate_successor_nodes();
-        System.out.println("IDS Output:\n");
+        //Dr. K --> USE A 3-MINUTE TIMER
+		long begin_timer = System.currentTimeMillis();
+        long end_timer = 180000;
+        boolean times_up = false;
+
+        //Generate Goal Node
+        Node goal_node = new Node(goal_position[0]-1, goal_position[1]-1);
+        //Generate Starting Node
+		Node start_node = new Node(starting_position[0]-1, starting_position[1]-1, manhattan_distance(starting_position[0]-1, starting_position[1]-1, goal_node), map_space[starting_position[0]-1][starting_position[1]-1], null);
+        boolean[][] nodes_visited = new boolean[map_space.length][map_space[0].length];
+        int total = 0, limit = 0, infitite_count = limit+1;      // might have to set to Integer.MAX_VALUE()
+        //REMEMBER TO USE STACK AS A FRINGE (FILO)
+        Stack<Node> the_fringe = new Stack<Node>();
+		queue_nodes.add(start_node);
+        
+        while(limit<infitite_count){
+            //Reset variables from previous search
+			falsify_visited_nodes(nodes_visited);
+			children_expanded = 0;
+            memory_nodes = 0;
+            //Build the stack
+            the_fringe.push(start_node);
+
+			while(!(the_fringe.isEmpty())){
+                //Start the 3-Minute timer
+                if((System.currentTimeMillis()-begin_timer) > end_timer){
+                    System.out.println("3 minutes have passed. Your search time has expired!");
+                    times_up = true;
+                    break;
+                }
+                
+				//Expand current node by poping from the stack
+				Node current_node = the_fringe.pop();
+				nodes_visited[current_node.x][current_node.y] = true;
+				children_expanded++;
+
+                //Check if this is a goal node
+			    if(is_goal_node(current_node)){
+                    int path_cost = current_node.accumulated_path_cost;
+                    display_path_information(current_node, path_cost);
+                    limit = infitite_count+1;        //Add limit for IDS
+                    System.out.println("Our path cost: "+path_cost);
+				    break;
+			    }
+                
+                //Check if we've reached our limit
+				if(total==limit){
+                    total++;
+					the_fringe.removeAllElements();     //Limit reached; reset stack
+					break;
+                }
+                
+                //Generate Successor Nodes && push to the fringe (stack)
+                Node[] children_nodes_array = generate_successor_nodes(current_node, goal_node, nodes_visited, map_space);
+				for(int i=0; i<children_nodes_array.length; i++){
+					the_fringe.push(children_nodes_array[i]);
+				}
+                
+                //Add sucessor nodes to our memory counter
+                if(memory_nodes<the_fringe.size())//add the children to count for nodes in memory
+                    memory_nodes=the_fringe.size();
+			}
+			total++;
+			infitite_count++;
+		}		
     }
+
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
     /**
@@ -133,14 +235,83 @@ public class Assignment2 {
      * @param int[][] map_space the 2D map we generated
 	 * This method implements A* Search on our tree from our text file.
 	 */
-    public static void a_star_search(int[][] map_space){        //Called from each search function
-        int man_distance = manhattan_distance();
-        //FIXME: The following call should take arguments similar to: generate_successor_nodes(Node current, Node g, boolean[][] visited, int[][] map)
-        //generate_successor_nodes();
+    public static void a_star_search(int[][] map_space){
+        //Dr. K --> USE A 3-MINUTE TIMER
+		long begin_timer = System.currentTimeMillis();
+        long end_timer = 180000;
+        boolean times_up = false;
+        //int man_distance = manhattan_distance();
 
+        /**
+         * TODO: Add A* Logic
+         */
+        
+        boolean[][] nodes_visited = new boolean[map_space.length][map_space[0].length];
+        Queue<Node> queue_nodes = new LinkedList<>();
+		queue_nodes.add(start_node);
+        Node[] child_node_array;
+
+        while(!(queue_nodes.isEmpty())){
+            //Start the 3-Minute timer
+            if((System.currentTimeMillis()-begin_timer) > end_timer){
+				System.out.println("3 minutes have passed. Your search time has expired!");
+				times_up = true;
+				break;
+			}
+
+            //Search && Expand the current node
+            Node current_node = queue_nodes.poll();                             //Removes + Returns head of queue 
+			nodes_visited[current_node.x][current_node.y] = true;               //Call .x & .y position in Node class
+			memory_nodes++;
+            
+            //Check if this is a goal node
+			if(is_goal_node(current_node)){
+                int path_cost = current_node.accumulated_path_cost;
+				display_path_information(current_node, path_cost);              //Display goal path information 
+				break;
+			}
+            
+            //If no goal exists, expand successor nodes & add them to the queue
+            //TODO IMPLEMENT THE GENERATION METHOD
+			children_nodes_array = generate_successor_nodes(current_node, goal_node, nodes_visited, map_space);
+			for(int i=0; i<children_nodes_array.length; i++){
+                queue_nodes.add(children_nodes_array[i]);
+            }
+
+            //Add sucessor nodes to memory
+			if(nodes_stored<queue_nodes.size())           //Fuck it; what if we just make all the variables statically available to the entire program?
+				nodes_stored = queue_nodes.size();
+        }
         System.out.println("Manhatten Distance\t" +manhattan_distance+ "\nA* Output:\n");
     }
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///////HELPER METHODS FOR SEARCH ALGORITHMS
+
+    //sets the visited matrix all to false
+    /**
+	 * SET VISITED NODES TO FALASE
+     * @param boolean[][] nodes_visited the nodes already explored
+     * Sets the visited nodes in 2D Mapspace to false
+	 */  
+    public static void falsify_visited_nodes(boolean[][] nodes_visited) {
+        for(int i=0; i<nodes_visited.length; i++)
+            for(int j=0; j<nodes_visited[i].length; j++)
+            nodes_visited[i][j] = false;
+    } 
+
+    /**
+	 * GOAL NODE CHECKER
+     * @param Node current_node the node we're currently at in the tree
+     * @return boolean indicates if we've reached our goal node
+	 */  
+    public static boolean is_goal_node(Node current_node){
+        if(current_node.distance == 0)
+            return true;
+        else
+            return false;
+    }
+
+    
     /**
 	 * MANHATTAN DISTANCE
 	 * This method calculates and @return the manhattan distance.
@@ -173,9 +344,23 @@ public class Assignment2 {
      * This method prints the path & cost from the goal node to the start node
      */
     public static Node[] generate_successor_nodes(Node current_node, Node goal_node, boolean[][] nodes_visited, int[][] map_space){
+
+        //We can only move UP, DOWN, LEFT, & RIGHT
         LinkedList<Node> children_nodes_LL = new LinkedList <Node> ();
 
-        //Return an Object Array -- Cast to type Array
+        //Move Up
+
+
+        //Move Down
+
+
+        //Move Left
+
+
+        //Move Right
+
+
+        //Return an Object Array --> Cast to type Array
         return children_nodes_LL.toArray();
     }
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
--- Node.java
+++ Node.java
@@ -2,7 +2,7 @@
  * CS 165a
  * Lab 1: Tree Searches
  * By: Matthew S Montoya && Hiram Rios
- * Last Modified: 26 February 2019
+ * Last Modified: 1 March 2019
  * Purpose: To practicing implementing BFS, IDS, & A* Search
  * NOTE: THIS IS THE NODE CLASS FOR SEARCHES
  */
@@ -13,22 +13,24 @@ public class Node {
     int distance;                       //Distance to goal node
 	int accumulated_path_cost;          //Cost from traversal (so far)
     Node prev;
-    
+    Node next;
 
     public Node(int x, int y){
         this.x = x;
         this.y = y;
 		distance = 0;
 		accumulated_path_cost = 0;
-		prev = null;
+        prev = null;
+        next = null;
     }
 
 
-    public Node(int x, int y, int distance, int accumulated_path_cost, Node prev){
+    public Node(int x, int y, int distance, int accumulated_path_cost, Node prev, Node next){
         this.x = x;
         this.y = y;
         this.distance = distance;
 		this.accumulated_path_cost = accumulated_path_cost;
-		this.prev = prev;
+        this.prev = prev;
+        this.next = next;
     }
 }
